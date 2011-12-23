7a9adf546ec237cc69e1b82754ad4a0051b58ae2
Julien Roncaglia <julien@roncaglia.fr>
2011-12-23 22:20:06 +0100

    Also minimize css

--- minimize.rb
+++ minimize.rb
@@ -3,23 +3,23 @@ require 'FileUtils'
 print 'Minimize (y/n) '
 minimize = /^(y|yes|1)$/.match(readline) ? true : false
 
-if File.exists?('tpl/_head_script.html')
-	FileUtils.rm('tpl/_head_script.html')
+if File.exists?('tpl/_head_x.html')
+	FileUtils.rm('tpl/_head_x.html')
 end
-	
+
 if minimize
+	# SCRIPTS
 	scripts = Dir.glob('js/*.js')
 	scripts = scripts.map { |s| "--js " + s }
 	
-	cmdLine = 'java -jar tools/closure-compiler.jar --js_output_file minimized.js ' + scripts.join(' ')
+	cmdLine = 'java -jar tools/closure-compiler.jar --js_output_file _minimized.js ' + scripts.join(' ')
 	puts cmdLine
-	
 	system(cmdLine)
 	
 	minimizedScripts = ['../default/js/jquery.js', '../default/js/jquery.cookie.js']
 	minimizedScripts.concat(Dir.glob('js/minimized/*.js'))
 	
-	open('minimized.js', 'a') { |file|
+	open('_minimized.js', 'a') { |file|
 		minimizedScripts.each { |otherFile|
 			open(otherFile).each { |line|
 				file.puts line
@@ -27,7 +27,17 @@ if minimize
 		}
 	}
 	
-	FileUtils.cp('tpl/_head_script_minimized.html', 'tpl/_head_script.html');
+	# CSS
+	cssFiles = Dir.glob('styles/*.css')
+	
+	open('_minimized.css', 'w') { |css|
+		cssFiles.each { |cssFile|
+			css.puts `java -jar tools/yuicompressor-2.4.7.jar --type css #{cssFile}`
+		}
+	}
+	
+	# Fix templates
+	FileUtils.cp('tpl/_head_x_minimized.html', 'tpl/_head_x.html');
 else
-	FileUtils.cp('tpl/_head_script_full.html', 'tpl/_head_script.html');
+	FileUtils.cp('tpl/_head_x_full.html', 'tpl/_head_x.html');
 end
